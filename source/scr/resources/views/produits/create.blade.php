0cc9bc59bf80dbebfb693bc96ee1e4bd1aecdaa7
amk <abdoulmalikkondi8@gmail.com>
2024-07-03 15:14:48 +0000

    init examen

---
+++ source/scr/resources/views/produits/create.blade.php
@@ -0,0 +1,49 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Commandes') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="bg-white flex items-center justify-between mx-6 px-6 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("Ajouter un nouveau produit") }}
+                </div>
+            </div>
+            <div class="bg-white flex items-center justify-between mx-6 px-6 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+               <div class="p-6 w-full space-y-6">
+                <form action="" method="post">
+                    @csrf
+                    <div class="space-y-6">
+                        <div class="flex space-x-3 items-center">
+                            <div class="space-y-2 w-1/3">
+                                <label for="">Libelle</label>
+                                <input type="text" name="" id="" class="border-gray-300 rounded-md w-full">
+                            </div>
+                            <div class="space-y-2 w-1/3">
+                                <label for="user">Prix</label>
+                                <input type="number" name="" id="" class="border-gray-300 rounded-md w-full">
+                            </div>
+                        </div>
+                        <div class="flex space-x-3 items-center">
+                            <div class="space-y-2 w-1/3">
+                                <label for="produit">Quantité</label>
+                                <input type="number" name="" id="" class="border-gray-300 rounded-md w-full">
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <button class="mt-6 bg-blue-600 hover:bg-blue-500 text-white text-sm px-3 py-2 rounded-md">
+                            Ajouter
+                        </button>
+                    </div>
+                </form>
+               </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
+
+
---
+++ source/scr/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+// use App\Http\Controllers\ProduitController;
+// use App\Http\Controllers\CommandeController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    /** Routes de ressource
+     * Vous créer pour les 7 méthode d'un controlleur les 7 routes automatiquement
+     * Syntaxe:
+     * Route::ressource('models', Controller::class)
+     */
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+
+require __DIR__.'/auth.php';
