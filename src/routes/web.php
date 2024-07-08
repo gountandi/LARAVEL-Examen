ebda3bcd50c6242828ffe8205652ea8ee03c0219
amk <abdoulmalikkondi8@gmail.com>
2024-07-08 12:11:51 +0000

    reinit projet setup

--- src/routes/web.php
+++ src/routes/web.php
@@ -20,6 +20,15 @@ Route::middleware('auth')->group(function () {
      * Route::ressource('models', Controller::class)
      */
 
+    Route::get('/commandes', function () {
+        return view("commandes.index");
+    })->name('commandes.index');
+
+    Route::get('/produits', function () {
+        return view("produits.index");
+    })->name('produits.index');
+
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
